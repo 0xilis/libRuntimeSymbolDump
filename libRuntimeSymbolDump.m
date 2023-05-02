aca4a8059d02055a1d1493df53cf7731a5715689
0xilis <109871561+0xilis@users.noreply.github.com>
2023-05-01 20:36:35 -0400

    libRuntimeSymbolDump.m

--- libRuntimeSymbolDump.m
+++ libRuntimeSymbolDump.m
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import "libRuntimeSymbolDump.h"
 
 //thank you to https://gist.github.com/domnikl/af00cc154e3da1c5d965
 void hexDumpByNSLog(const char *desc, void *addr, int len) {
@@ -24,6 +25,31 @@ void hexDumpByNSLog(const char *desc, void *addr, int len) {
   outputLog = [outputLog stringByAppendingString:[NSString stringWithFormat:@" %02x", pc[i]]];
  }
  NSLog(@"%@",outputLog);
+}
+
+NSString* hexDumpLogOutput(const char *desc, void *addr, int len) {
+ int i;
+ unsigned char *pc = (unsigned char*)addr;
+ NSString* outputLog = @"(libRSD)";
+
+ // Output description if given.
+ if (desc != NULL) {
+  outputLog = [outputLog stringByAppendingString:[NSString stringWithFormat:@"%s:\n", desc]];
+ }
+
+ // Process every byte in the data.
+ for (i = 0; i < len; i++) {
+  // Multiple of 16 means new line (with line offset).
+  if ((i % 16) == 0) {
+   // Just don't print ASCII for the zeroth line.
+   if (i != 0) {
+    outputLog = [outputLog stringByAppendingString:[NSString stringWithFormat:@"\n"]];
+   }
+  }
+  // Now the hex code for the specific character.
+  outputLog = [outputLog stringByAppendingString:[NSString stringWithFormat:@" %02x", pc[i]]];
+ }
+ return outputLog;
 }
 
 void hexDumpSymbolFromCallStackSymbols(NSString *symbolToFind) {
@@ -97,9 +123,47 @@ void* findSymbolPtrFromCallStackSymbols(NSString *symbolToFind) {
    [scanner scanHexLongLong:&jmpAddress];
    //result is our ptr to jmp address, substract offset from it to get symbol address
    void *fptr = (void *)(jmpAddress - jmpOffset);
+   NSLog(@"(libRSD)ran findSymbolPtrFromCallStackSymbols()\n");
    return fptr;
   }
  }
  NSLog(@"(libRSD)ran findSymbolPtrFromCallStackSymbols()\n");
  return -1;
+}
+
+NSArray<CallStackSymbol *>* callStackSymbolsToObject(void) {
+ NSArray<NSString *> * callstack = [NSThread callStackSymbols];
+ NSMutableArray<CallStackSymbol *> * mutableCallStackSymbolObjs = [[NSMutableArray alloc]init];
+
+ for (NSString* symbolString in callstack) {
+  CallStackSymbol * stackSymbolObj = [[CallStackSymbol alloc]init];
+  //NSCaseInsensitiveSearch / NSBackwardsSearch
+  NSRange range = [symbolString rangeOfString:@"0x" options:NSBackwardsSearch];
+  NSString* temp1 = [symbolString substringFromIndex:range.location];
+  NSRange range2 = [temp1 rangeOfString:@" " options:NSCaseInsensitiveSearch];
+  NSString* symbolJmpAddr = [temp1 substringToIndex:range2.location]; //symbol addr, well not addr of symbol but where the jmp in the symbol was
+  [stackSymbolObj setAddr:symbolJmpAddr];
+  
+  //symbol name
+  NSString *temp3 = [temp1 substringFromIndex:(range2.location + 1)]; // +1 to get rid of nasty extra space
+  NSRange range3 = [temp3 rangeOfString:@" + " options:NSBackwardsSearch];
+  NSString *symbolName = [temp3 substringToIndex:range3.location];
+  [stackSymbolObj setSymbolName:symbolName];
+  
+  //the position / offset of where the jmp is in the symbol; (symboladdr + jmp offset = where it jmped to new function)
+  long jmpOffset = [[temp3 substringFromIndex:(range3.location + 3)] integerValue];
+  NSLog(@"(libRSD)symbol jmp offset: %ld", jmpOffset);
+  [stackSymbolObj setPosition:jmpOffset];
+
+  //calculate the pointer to the actual symbol
+  unsigned long long jmpAddress;
+  NSScanner* scanner = [NSScanner scannerWithString:symbolJmpAddr];
+  [scanner scanHexLongLong:&jmpAddress];
+  //result is our ptr to jmp address, substract offset from it to get symbol address
+  void *fptr = (void *)(jmpAddress - jmpOffset);
+  [stackSymbolObj setSymbolPointer:fptr];
+  [mutableCallStackSymbolObjs addObject:stackSymbolObj];
+ }
+ NSLog(@"(libRSD)ran callStackSymbolsToObject()\n");
+ return [[NSArray alloc]initWithArray:mutableCallStackSymbolObjs];
 }
